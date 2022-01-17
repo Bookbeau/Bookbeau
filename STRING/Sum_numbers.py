6d1fd0c8fb2a417a28c5153474db72a5d814a3f6
Bookbeau <97019682+Bookbeau@users.noreply.github.com>
2022-01-17 15:29:45 +0530

    Add files via upload

---
+++ STRING/Sum_numbers.py
@@ -0,0 +1,6 @@
+#Write a python program to sum all the numbers in a list.
+list1=[11,5,17,18,23]
+#to use sum() function in program
+total=sum(list1)
+#printing total value of a list
+print("Sum of all elements in given list:",total)
